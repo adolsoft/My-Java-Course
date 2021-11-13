a314aeb86a6cb8fe8e1c18cd7355de801a7244ff
adolsoft <75003852+adolsoft@users.noreply.github.com>
2021-11-13 23:48:09 +0100

    ForLoop denemeleri

---
+++ day17_forLoops/C02_FortLoop.java
@@ -0,0 +1,25 @@
+package day17_forLoops;
+
+import java.util.Scanner;
+
+public class C02_FortLoop {
+
+	public static void main(String[] args) {
+		// Soru 7 ) Interview Question Kullanicidan bir String isteyin ve Stringi tersten yazdirin. 
+	    
+        Scanner scan = new Scanner(System.in);
+        System.out.println("lutfen tersten yazdirmak icin bir yazi girin...");
+        String str = scan.nextLine();
+        
+        // Fatih 
+        
+        System.out.println(str.length()-1);
+        
+        for (int i = str.length()-1 ; i >=0 ; i--) {
+            
+            System.out.print(str.substring(i,i+1));
+            
+        }
+        
+    }
+}
